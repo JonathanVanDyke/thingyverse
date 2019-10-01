387ddaba8aa041695619bf1e72ae566f580afc25
Jonathan <jfv213@gmail.com>
2019-10-01 14:40:52 -0400

    Create page and content styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -12,20 +12,54 @@ import SignUpFormContainer from './session_form/signup_form_container';
 import LogInFormContainer from './session_form/login_form_container';
 import Splash from './Splash'
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import styled from 'styled-components';
 
+const Content = styled.div({
+  position: 'absolute',
+  display: 'flex',
+  flexDirection: 'column',
+  alignContent: 'center',
+  width: 971,
+  height: '100%',
+  background: '#f5f5f5',
+  // border: '1px solid red'
+});
+
+const Page = styled.div({
+  height: '100%',
+  width: '100%',
+  position: 'absolute',
+  display: 'flex',
+  justifyContent: 'center',
+  background: '#f5f5f5',
+  // border: '1px solid red'
+});
+
+const Nav = styled.nav({
+  position: 'absolute',
+  width: '100%',
+  height: 60,
+  // background: '#f1f1f1',
+  backgroundImage: 'linear-gradient(to bottom, #f5f5f5, #e8e8e8)',
+  borderBottom: '1px solid #bebebe',
+})
+
+//236px margin on left and right...
 const App = () => (
-  <div>
-    <header>
-      <Link to="/" className="header-link">
-        <h1>Thingyverse</h1>
-      </Link>
-    </header>
-    <Switch>
-      <Route exact path="/" component={Splash} />
-      <AuthRoute exact path="/login" component={LogInFormContainer} />
-      <AuthRoute exact path="/signup" component={SignUpFormContainer} />
-    </Switch>
-  </div>
+  <Page>
+    <Nav>
+
+    </Nav>
+    <Content>
+      <header>
+        <Route exact path="/" component={Splash} />
+      </header>
+      <Switch>
+        <AuthRoute exact path="/login" component={LogInFormContainer} />
+        <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+      </Switch>
+    </Content>
+  </Page>
 );
 
 export default App;
