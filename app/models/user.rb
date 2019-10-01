a43f50d1fb07a5dbc292a441de1f71d31268593d
Jonathan <jfv213@gmail.com>
2019-10-01 17:02:52 -0400

    Fixed typo in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
 
   has_many :comments
 
-  def self.find_by_user_credentials(username, password)
+  def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return nil unless user && user.is_password?(password)
     user
