b5490cea7a15b62e7e437f73e1247fdc62c31e58
Jonathan <jfv213@gmail.com>
2019-09-30 22:36:03 -0400

    Create app and root components

---
+++ frontend/components/app.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+import SignUpFormContainer from './session_form/signup_form_container';
+import LogInFormContainer from './session_form/login_form_container';
+import SplashContainer from './splash/splash_container'
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
+const App = () => (
+  <div>
+    <header>
+      <Link to="/" className="header-link">
+        <h1>Thingyverse</h1>
+      </Link>
+    </header>
+    <Switch>
+      <Route exact path="/" component={SplashContainer} />
+      <AuthRoute exact path="/login" component={LogInFormContainer} />
+      <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+    </Switch>
+  </div>
+);
+
+export default App;
