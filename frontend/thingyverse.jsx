dcb2f4e3864b2a694a3b9840bf6cf24900904b32
Jonathan <jfv213@gmail.com>
2019-10-01 22:46:53 -0400

    Update for heroku push

--- frontend/thingyverse.jsx
+++ frontend/thingyverse.jsx
@@ -20,9 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-  //testing
-  window.getState = store.getState;
-  //testing
+
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 });
