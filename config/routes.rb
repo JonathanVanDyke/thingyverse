7298ab474bfe2df795876a183f972860b9ed82b1
Jonathan <jfv213@gmail.com>
2019-10-03 17:55:00 -0400

    Updated routes to include prints

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :user, only: [:create, :show]
     resource :session, only: [:create, :destroy, :show]
+    resources :prints, only: [:create, :show, :destroy]
   end
 
   root "static_pages#root"
